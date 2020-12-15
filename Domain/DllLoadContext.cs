219ef89f096068299af69f14d857a3d624764931
EkkiRu <63056889+EkkiRu@users.noreply.github.com>
2020-12-15 16:02:18 +0600

    Homework
    Homework

---
+++ Domain/DllLoadContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.Loader;
+
+namespace DomainHomeWork
+{
+    public class DllLoadContext : AssemblyLoadContext
+    {
+        public DllLoadContext() : base(true)
+        {
+
+        }
+
+        protected override Assembly Load(AssemblyName assemblyName)
+        {
+            return null;
+        }
+    }
+}
---
+++ Domain/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Http.Headers;
+
+namespace DomainHomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExecuteAndUnload("Your code: 0159");
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            Console.Read();
+        }
+
+        static void ExecuteAndUnload(string text)
+        {
+            var context = new DllLoadContext();
+
+            var smsAssembly = context.LoadFromAssemblyPath(@"C:\Users\Mephistos\source\repos\SmsLibrary\bin\Debug\netcoreapp3.1\SmsLibrary.dll");
+
+            var classType = smsAssembly.GetType("SmsLibrary.SmsMessageSender");
+            var smsMethodInfo = classType.GetMethod("SendMessage");
+
+            var classObject = Activator.CreateInstance(classType);
+            var result = smsMethodInfo.Invoke(classObject, new object[]  { text } );
+
+            Console.WriteLine(result);
+
+            context.Unload();
+        }
+    }
+}
